c5546cfbc76a6588193a8bc0a53642def493159e
benjamanonni <161636221+benjamanonni@users.noreply.github.com>
2026-04-22 11:53:01 -0300

    Update ConexionBD.java

--- Prestamos/src/main/java/CONEXION/ConexionBD.java
+++ Prestamos/src/main/java/CONEXION/ConexionBD.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class ConexionBD {
     private static final String URL =
             "jdbc:mysql://localhost:3306/SISTEMAPRESTAMO";
-    private static final String USER = "app_prestamos";
-    private static final String PASSWORD ="AppPrestamos123!";
+    private static final String USER = "usuario";
+    private static final String PASSWORD ="contraseña";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
